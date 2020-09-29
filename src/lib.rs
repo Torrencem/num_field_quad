376e6bbcc59a4a1d195cc40eb66141e139a83b2b
Matthew Torrence <matt@torrencefamily.net>
2020-09-29 11:30:10 -0400

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,271 @@
+
+#[macro_use]
+extern crate lazy_static;
+extern crate derive_more;
+
+mod utils;
+
+use utils::*;
+use num_traits::PrimInt;
+
+pub trait MyPrimInt: PrimInt + std::ops::AddAssign + std::ops::SubAssign + std::fmt::Debug { }
+impl<T: PrimInt + std::ops::AddAssign + std::ops::SubAssign + std::fmt::Debug> MyPrimInt for T { }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct QuadraticField<Int: MyPrimInt> {
+    pub c: Int,
+}
+
+impl<Int: MyPrimInt> QuadraticField<Int> {
+    // Returns None if c is a perfect square
+    pub fn from_c(c: Int) -> Option<QuadraticField<Int>> {
+        if c >= Int::zero() && utils::is_perfect_square(c).is_some() {
+            None
+        } else {
+            Some(QuadraticField { c })
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct QFElement<Int: MyPrimInt> {
+    // The standard representation of the element
+    a: Int,
+    b: Int,
+    d: Int,
+    // The underlying field
+    field: QuadraticField<Int>,
+}
+
+impl<Int: MyPrimInt + std::fmt::Display> std::fmt::Display for QFElement<Int> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // TODO handle special cases like a0 or a1 being 0, d being 1 and c being -1
+        write!(f, "({} + {}√{})/({})", self.a, self.b, self.field.c, self.d)
+    }
+}
+
+impl<Int: MyPrimInt> QFElement<Int> {
+    pub fn from_integer(int: Int, field: QuadraticField<Int>) -> QFElement<Int> {
+        QFElement {
+            a: int,
+            b: Int::zero(),
+            d: Int::one(),
+            field,
+        }
+    }
+
+    pub fn from_rational(a: Int, b: Int, field: QuadraticField<Int>) -> QFElement<Int> {
+        QFElement {
+            a,
+            b: Int::zero(),
+            d: b,
+            field,
+        }
+    }
+
+    pub fn from_parts(a0: Int, a1: Int, d: Int, field: QuadraticField<Int>) -> QFElement<Int> {
+        QFElement {
+            a: a0, b: a1, d, field,
+        }
+    }
+
+    pub fn is_rational(&self) -> bool {
+        self.b.is_zero()
+    }
+    
+    // Get the standard representation as defined in section 4.2.2.
+    pub fn standard_representation(&self) -> (Int, Int, Int) {
+        (self.a, self.b, self.d)
+    }
+
+    // Find the multiplicative inverse of this element
+    pub fn inverse(&self) -> Self {
+        let my_poly = QuadPoly { a: Int::zero(), b: self.a, c: self.b };
+        let t = QuadPoly { a: Int::one(), b: Int::zero(), c: self.field.c };
+        let (_v, u, gcd) = poly_extended_gcd(t, my_poly);
+        if gcd.degree() != 0 {
+            dbg!(t);
+            dbg!(my_poly);
+            dbg!(gcd);
+        }
+        assert!(gcd.degree() == 0);
+        dbg!(gcd);
+        QFElement {
+            a: u.b * self.d,
+            b: u.c * self.d,
+            d: Int::zero() - gcd.c,
+            field: self.field,
+        }.reduce()
+    }
+
+    pub fn reduce(mut self) -> Self {
+        let g = gcd(self.a, gcd(self.b, self.d));
+        self.a = self.a / g;
+        self.b = self.b / g;
+        self.d = self.d / g;
+        self
+    }
+}
+
+impl<Int: MyPrimInt> std::ops::Add for QFElement<Int> {
+    type Output = QFElement<Int>;
+
+    fn add(self, other: QFElement<Int>) -> Self::Output {
+        assert!(self.field == other.field);
+        let new_d = lcm(self.d, other.d);
+        QFElement {
+            a: self.a * new_d / self.d + other.a * new_d / other.d,
+            b: self.b * new_d / self.d + other.b * new_d / other.d,
+            d: new_d,
+            field: self.field,
+        }
+    }
+}
+
+impl<Int: MyPrimInt> std::ops::Sub for QFElement<Int> {
+    type Output = QFElement<Int>;
+
+    fn sub(self, other: QFElement<Int>) -> Self::Output {
+        assert!(self.field == other.field);
+        let new_d = lcm(self.d, other.d);
+        QFElement {
+            a: self.a * new_d / self.d - other.a * new_d / other.d,
+            b: self.b * new_d / self.d - other.b * new_d / other.d,
+            d: new_d,
+            field: self.field,
+        }
+    }
+}
+
+impl<Int: MyPrimInt> std::ops::Mul for QFElement<Int> {
+    type Output = QFElement<Int>;
+
+    fn mul(self, other: QFElement<Int>) -> Self::Output {
+        assert!(self.field == other.field);
+        QFElement {
+            a: self.a * other.a + self.a * other.a * self.field.c,
+            b: self.a * other.b + self.b * other.a,
+            d: self.d * other.d,
+            field: self.field,
+        }.reduce()
+    }
+}
+
+impl<Int: MyPrimInt> std::ops::Add<Int> for QFElement<Int> {
+    type Output = QFElement<Int>;
+
+    fn add(mut self, rhs: Int) -> Self::Output {
+        self.a = self.a + self.d * rhs;
+        self
+    }
+}
+
+impl<Int: MyPrimInt> std::ops::Sub<Int> for QFElement<Int> {
+    type Output = QFElement<Int>;
+
+    fn sub(mut self, rhs: Int) -> Self::Output {
+        self.a = self.a - self.d * rhs;
+        self
+    }
+}
+
+impl<Int: MyPrimInt> std::ops::Mul<Int> for QFElement<Int> {
+    type Output = QFElement<Int>;
+
+    fn mul(mut self, rhs: Int) -> Self::Output {
+        self.a = self.a * rhs;
+        self.b = self.b * rhs;
+        self
+    }
+}
+
+impl<Int: MyPrimInt> std::ops::Div<Int> for QFElement<Int> {
+    type Output = QFElement<Int>;
+
+    fn div(mut self, rhs: Int) -> Self::Output {
+        self.d = self.d * rhs;
+        self
+    }
+}
+
+impl<Int: MyPrimInt> PartialEq<Int> for QFElement<Int> {
+    fn eq(&self, other: &Int) -> bool {
+        self.b.is_zero() && self.a / self.d == *other
+    }
+}
+
+// TODO: This probably shouldn't use unwrap()
+#[macro_export]
+macro_rules! qfelement {
+    (($a:expr) + ($b:expr)i @($c:expr)) => ({
+        QFElement::from_parts($a, $b, 1, $c)
+    });
+    (($c:expr) ! ($a:expr)) => ({
+        QFElement::from_parts($a, 0, 1, $c)
+    });
+    (($a:expr) + ($b:expr)sqrt($c:expr)) => ({
+        QFElement::from_parts($a, $b, 1, QuadraticField::from_c($c).unwrap())
+    });
+    (($a:expr) + sqrt($c:expr)) => ({
+        QFElement::from_parts($a, 1, 1, QuadraticField::from_c($c).unwrap())
+    });
+    (($a:expr)sqrt($c:expr)) => ({
+        QFElement::from_parts(0, $a, 1, QuadraticField::from_c($c).unwrap())
+    });
+    (sqrt($c:expr)) => ({
+        QFElement::from_parts(0, 1, 1, QuadraticField::from_c($c).unwrap())
+    });
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_macro_uses() {
+        let qi = QuadraticField::from_c(-1).unwrap();
+        
+        // 5 + 6i
+        let val = qfelement!((5) + (6)i @ (qi));
+
+        assert_eq!(val, QFElement::from_parts(5, 6, 1, qi));
+        
+        // 5
+        let val = qfelement!((qi) ! (5));
+
+        assert_eq!(val, QFElement::from_parts(5, 0, 1, qi));
+        
+        // -10 + 5i
+        let val = qfelement!((-10) + (5)sqrt(-1));
+
+        assert_eq!(val, QFElement::from_parts(-10, 5, 1, qi));
+        
+        // 2i
+        let val = qfelement!((2)sqrt(-1));
+
+        assert_eq!(val, QFElement::from_parts(0, 2, 1, qi));
+    
+        // i
+        let val = qfelement!(sqrt(-1));
+        
+        assert_eq!(val, QFElement::from_parts(0, 1, 1, qi));
+
+        // An example of creating an actual rational number
+        
+        // The golden ratio (1 + sqrt(5)/2
+        let phi = qfelement!((1) + sqrt(5)) / 2;
+
+        assert!(phi * phi - phi - 1 == 0);
+    }
+
+    #[test]
+    fn test_mul_inverse() {
+        let phi = qfelement!((1) + sqrt(5)) / 2;
+        assert_eq!(phi.inverse().inverse(), phi);
+        assert_eq!(phi * phi.inverse(), qfelement!((1) + (0)sqrt(5)));
+
+        let val = qfelement!((2) + (5)sqrt(-1));
+        dbg!(val.inverse());
+    }
+}
---
+++ src/utils.rs
@@ -0,0 +1,342 @@
+
+use num_traits::PrimInt;
+
+use derive_more::*;
+// use derive_more::{PartialEq, Add, Sub, Neg, AddAssign, SubAssign};
+
+/// A general quadratic polynomial
+/// ax^2 + bx + c
+#[derive(Debug, Clone, Copy, PartialEq, Add, Sub, Neg, AddAssign, SubAssign)]
+pub struct QuadPoly<Int: PrimInt> {
+    pub a: Int,
+    pub b: Int,
+    pub c: Int,
+}
+
+impl<Int: PrimInt> QuadPoly<Int> {
+    pub fn degree(&self) -> usize {
+        if !self.a.is_zero() {
+            2
+        } else if !self.b.is_zero() {
+            1
+        } else {
+            0
+        }
+    }
+
+    pub fn lc(&self) -> Int {
+        if !self.a.is_zero() {
+            self.a
+        } else if !self.b.is_zero() {
+            self.b
+        } else {
+            self.c
+        }
+    }
+
+    pub fn cont(&self) -> Int {
+        gcd(self.a, gcd(self.b, self.c))
+    }
+
+    pub fn is_zero(&self) -> bool {
+        self.a.is_zero() && self.b.is_zero() && self.c.is_zero()
+    }
+
+    pub fn constant(val: Int) -> Self {
+        QuadPoly {
+            a: Int::zero(),
+            b: Int::zero(),
+            c: val,
+        }
+    }
+}
+
+impl<Int: PrimInt> std::ops::Mul for QuadPoly<Int> {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+        // Only multiplies if the result is a quadratic polynomial
+        match (self.degree(), other.degree()) {
+            (_, 0) => { self * other.c },
+            (0, _) => { other * self.c },
+            (1, 1) => QuadPoly { a: self.b * other.b, b: self.b * other.c + self.c * other.b, c: self.c * other.c },
+            _ => panic!("Invalid quadratic polynomial multiplication"),
+        }
+    }
+}
+
+impl<Int: PrimInt> std::ops::Mul<Int> for QuadPoly<Int> {
+    type Output = Self;
+
+    fn mul(mut self, rhs: Int) -> Self::Output {
+        self.a = self.a * rhs;
+        self.b = self.b * rhs;
+        self.c = self.c * rhs;
+        self
+    }
+}
+
+impl<Int: PrimInt> std::ops::Div<Int> for QuadPoly<Int> {
+    type Output = Self;
+
+    fn div(mut self, rhs: Int) -> Self::Output {
+        self.a = self.a / rhs;
+        self.b = self.b / rhs;
+        self.c = self.c / rhs;
+        self
+    }
+}
+
+// /// A quadratic polynomial of the form x^2 + c for
+// /// some integer c
+// struct SimpleQuadPoly<Int: PrimInt> {
+//     c: Int,
+// }
+
+pub fn gcd<Int: PrimInt>(mut a: Int, mut b: Int) -> Int {
+    if a < b {
+        std::mem::swap(&mut a, &mut b);
+    }
+    while !b.is_zero() {
+        a = a % b;
+        std::mem::swap(&mut a, &mut b);
+    }
+    a
+}
+
+pub fn lcm<Int: PrimInt>(a: Int, b: Int) -> Int {
+    // quick optimization
+    if a == b {
+        a
+    } else {
+        a * b / gcd(a, b)
+    }
+}
+
+// Algorithm 3.1.2
+pub fn poly_pseudo_div<Int: PrimInt + std::ops::AddAssign + std::ops::SubAssign>(a_poly: QuadPoly<Int>, b_poly: QuadPoly<Int>) -> (QuadPoly<Int>, QuadPoly<Int>) {
+    let mut r = a_poly;
+    let mut q = QuadPoly {
+        a: Int::zero(),
+        b: Int::zero(),
+        c: Int::zero(),
+    };
+    let mut e = a_poly.degree() - b_poly.degree() + 1;
+    let d = b_poly.lc();
+
+    while r.degree() >= b_poly.degree() {
+        let x_degr_minus_degd = match r.degree() - b_poly.degree() {
+            0 => QuadPoly { a: Int::zero(), b: Int::zero(), c: Int::one() },
+            1 => QuadPoly { a: Int::zero(), b: Int::one(), c: Int::zero() },
+            2 => QuadPoly { a: Int::one(), b: Int::zero(), c: Int::zero() },
+            _ => unreachable!()
+        };
+        let s = x_degr_minus_degd * (r.lc());
+        let sb = match x_degr_minus_degd.degree() {
+            0 => QuadPoly {
+                a: b_poly.a * (r.lc()), b: b_poly.b * (r.lc()), c: b_poly.c * (r.lc()),
+            },
+            1 => QuadPoly {
+                a: b_poly.b * (r.lc()), b: b_poly.c * (r.lc()), c: Int::zero(),
+            },
+            2 => QuadPoly {
+                a: b_poly.c * (r.lc()), b: Int::zero(), c: Int::zero(),
+            },
+            _ => unreachable!()
+        };
+        q = (q * d) + s;
+        r = (r * d) - sb;
+        if e != 0 {
+            e = e - 1;
+        }
+    }
+
+    (q * d.pow(e as u32), r * d.pow(e as u32))
+}
+
+// Algorithm 3.3.1
+pub fn poly_gcd<Int: PrimInt + std::ops::AddAssign + std::ops::SubAssign>(mut a_poly: QuadPoly<Int>, mut b_poly: QuadPoly<Int>) -> QuadPoly<Int> {
+    // 1. Initializations and reductions
+    if b_poly.degree() > a_poly.degree() {
+        std::mem::swap(&mut a_poly, &mut b_poly)
+    }
+    if b_poly.is_zero() {
+        return a_poly;
+    }
+    let a = a_poly.cont();
+    let b = b_poly.cont();
+    let d = gcd(a, b);
+    a_poly = a_poly / a;
+    b_poly = b_poly / b;
+    let mut g = Int::one();
+    let mut h = Int::one();
+    
+    loop {
+        // 2. Pseudo division
+        let delta = a_poly.degree() - b_poly.degree();
+
+        let (_q, r) = poly_pseudo_div(a_poly * b_poly.lc().pow(delta as u32 + 1), b_poly);
+
+        if r.is_zero() {
+            // 4. Terminate
+            return (b_poly / b_poly.cont()) * d;
+        }
+        if r.degree() == 0 {
+            // 4. Terminate
+            return QuadPoly::constant(d / b_poly.cont())
+        }
+
+        // 3. Reduce remainder
+        a_poly = b_poly;
+        b_poly = r / (g * h.pow(delta as u32));
+        g = a_poly.lc();
+        if delta != 0 {
+            h = g.pow(delta as u32) / h.pow(delta as u32 - 1);
+        }
+    }
+}
+
+// https://en.wikipedia.org/wiki/Polynomial_greatest_common_divisor#B%C3%A9zout%27s_identity_and_extended_GCD_algorithm
+pub fn poly_extended_gcd<Int: PrimInt + std::ops::AddAssign + std::ops::SubAssign + std::fmt::Debug>(a_poly: QuadPoly<Int>, b_poly: QuadPoly<Int>) -> (QuadPoly<Int>, QuadPoly<Int>, QuadPoly<Int>) {
+    assert!(a_poly.degree() >= b_poly.degree());
+    let mut r_prev = a_poly;
+    let mut r = b_poly;
+    let mut s_prev = QuadPoly::constant(Int::one());
+    let mut s = QuadPoly::constant(Int::zero());
+    let mut t_prev = QuadPoly::constant(Int::zero());
+    let mut t = QuadPoly::constant(Int::one());
+    
+    // TODO: This should only work for "coprime" polynomials, this should be !r_prev.is_zero()
+    // (or something like that)
+    // while r.degree() != 0 {
+    while !r.is_zero() {
+        dbg!(r_prev);
+        dbg!(r);
+        let (q, _) = poly_pseudo_div(r_prev, r);
+        let d = r.lc();
+        let e = r_prev.degree() - r.degree() + 1;
+        let new_r = (r_prev * d.pow(e as u32)) - q * r;
+        let new_s = (s_prev * d.pow(e as u32)) - q * s;
+        let new_t = (t_prev * d.pow(e as u32)) - q * t;
+        r_prev = r;
+        r = new_r;
+        s_prev = s;
+        s = new_s;
+        t_prev = t;
+        t = new_t;
+    }
+
+    // (u, v, gcd)
+    (s, t, r)
+}
+
+lazy_static! {
+    // Precomputations 1.7.2
+    static ref Q11: [bool; 11] = {
+        let mut table: [bool; 11] = [false; 11];
+        for k in 0..=5 {
+            table[k * k % 11] = true;
+        }
+        table
+    };
+
+    static ref Q63: [bool; 63] = {
+        let mut table: [bool; 63] = [false; 63];
+        for k in 0..=31 {
+            table[k * k % 63] = true;
+        }
+        table
+    };
+
+    static ref Q64: [bool; 64] = {
+        let mut table: [bool; 64] = [false; 64];
+        for k in 0..=31 {
+            table[k * k % 64] = true;
+        }
+        table
+    };
+
+    static ref Q65: [bool; 65] = {
+        let mut table: [bool; 65] = [false; 65];
+        for k in 0..=32 {
+            table[k * k % 65] = true;
+        }
+        table
+    };
+}
+
+// Algorithm 1.7.1
+fn int_sqrt<Int: PrimInt>(val: Int) -> Int {
+    let mut x = val;
+    loop {
+        let y = (x + val / x) >> 2;
+        if y < x {
+            x = y;
+        } else {
+            return x;
+        }
+    }
+}
+
+pub fn is_perfect_square<Int: PrimInt>(val: Int) -> Option<Int> {
+    if Q64[(val % Int::from(64).unwrap()).to_usize().unwrap()] == false {
+        return None;
+    }
+    let r = val % Int::from(45045).unwrap();
+    if Q63[r.to_usize().unwrap() % 63] == false {
+        return None;
+    } else if Q65[r.to_usize().unwrap() % 65] == false {
+        return None;
+    } else if Q11[r.to_usize().unwrap() % 11] == false {
+        return None;
+    }
+    let q = int_sqrt(val);
+    if q * q != val {
+        None
+    } else {
+        Some(q)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let a = QuadPoly {
+            a: 1i32,
+            b: -3,
+            c: -10,
+        };
+
+        let b = QuadPoly {
+            a: 0i32,
+            b: 1,
+            c: 2,
+        };
+
+        assert_eq!(poly_pseudo_div(a, b), (QuadPoly { a: 0, b: 1, c: -5 }, QuadPoly { a: 0, b: 0, c: 0 }));
+
+
+        // (2x - 10) (x + 10)
+        let a = QuadPoly {
+            a: 2i32,
+            b: 10,
+            c: -100,
+        };
+
+        // (x + 10)(4 - x)
+        let b = QuadPoly {
+            a: -1i32,
+            b: -6,
+            c: 40,
+        };
+
+        let g = poly_gcd(a, b);
+
+        assert!(g == QuadPoly { a: 0, b: 1, c: 10 } || g == QuadPoly { a: 0, b: -1, c: -10 });
+    }
+}
+
